ae01c35b756653a7545c45d5d0fbc252cc53d85e
Sithara Azeez <sitharasithara2@gmail.com>
2022-06-20 21:36:55 +0530

    static file done

--- static sample/src/Statics.java
+++ static sample/src/Statics.java
@@ -1,4 +1,29 @@
 
 public class Statics {
+	int a=10;
+	static int b=50;
+public static void main(String a[]) {
+	
+	
+	
+	Statics h=new Statics();
+	Statics h1=new Statics();
+	hai();
+	h.hello();
+	h1.a=90;
+	b=100;
+	System.out.println("h.a:"+h.a);
+	
+	System.out.println("h1.a:"+h1.a);
+	System.out.println(h1.b);
+	System.out.println(h.b);
+	
+}
+void hello() {
+	System.out.println("hello");
+}
+static void hai() {
+	
+}
 
 }
