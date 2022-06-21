940cbc1b40a68035d9e6ca929d76708577c43531
Sithara Azeez <sitharasithara2@gmail.com>
2022-06-21 20:03:17 +0530

    this keyword- sample program done

---
+++ This sample/src/Sample.java
@@ -0,0 +1,11 @@
+
+public class Sample {
+	int a=100,b=500;
+	Sample(int a,int b){
+				/* it will print local variable took from class Hello */
+		this.a=a;
+		this.b=b;
+		System.out.println(this.a+" "+this.b);
+	}
+
+}
