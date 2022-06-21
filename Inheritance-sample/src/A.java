b4e2f830ad509e7565dd87c1781874bfc3370f88
Sithara Azeez <sitharasithara2@gmail.com>
2022-06-21 20:44:10 +0530

    inheritance-sample program done

---
+++ Inheritance-sample/src/A.java
@@ -0,0 +1,8 @@
+
+public class A {
+	int a;
+	void display() {
+		System.out.println("hai welcome class A");
+	}
+
+}
---
+++ Inheritance-sample/src/B.java
@@ -0,0 +1,8 @@
+
+public class B extends A {
+	int num;
+	void displayB() {
+		System.out.println("welcom B");
+	}
+
+}
