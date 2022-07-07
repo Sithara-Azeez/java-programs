02927e6f6bc47259fd3eb4e0512a79379b5c68ed
Sithara Azeez <sitharasithara2@gmail.com>
2022-07-08 02:25:54 +0530

    Package completed

---
+++ 9.Package-Sample/src/hello/hai/Sample.java
@@ -0,0 +1,15 @@
+package hello.hai;
+
+import hello.hey.Hello;
+public class Sample {
+	Sample(){
+		Hello h = new Hello();
+	}
+	Sample s = new Sample();
+	
+	void displayHello() {
+		System.out.println("Helloo...!!");
+	}
+	
+
+}
---
+++ 9.Package-Sample/src/hello/hey/Hello.java
@@ -0,0 +1,7 @@
+package hello.hey;
+public class Hello {
+	void displayHello() {
+		System.out.println("Helloo...!!");
+	}
+
+}
